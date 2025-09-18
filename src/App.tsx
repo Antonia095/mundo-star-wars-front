c8501e7ba9fef7e30d741d19ab7760314e22de2d
Antonia Maciel <antonia.josy95@gmail.com>
2025-09-18 11:29:06 -0300

    cria estruturacao do projeto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AppRoutes from './routes';
+import './styles/App.css'
+
+function App() {
+  return (
+   <AppRoutes />
+  );
+}
+
+export default App
+
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Star Wars Universe</h1>
+    </div>
+  );
+};
+
+export default Home;
