ea1a5a6ae8923090cfb7f5704af8d706d8fc33e8
Antonia Maciel <antonia.josy95@gmail.com>
2025-09-26 07:41:11 -0300

    adiciona pagina sobre

---
+++ src/pages/Sobre.tsx
@@ -0,0 +1,14 @@
+import '../styles/pages/Sobre.css';
+
+const Sobre = () => {
+  return (
+    <div className="sobre-container">
+      <h1>Mundo Star Wars</h1>
+      <p>
+        Este site exibe informações do universo Star Wars para fãs e curiosos.
+      </p>
+    </div>
+  );
+};
+
+export default Sobre;
